fa1a9cbe0b91efec02516a7f2871af25f730255a
Stefan Milic <stefan.milic@diwanee.com>
2018-02-23 10:26:44 +0100

    blank template

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <h2>{{getName}}</h2>
+  </div>
+</template>
+
+<script>
+//import { FETCH_POSTS } from './store/modules/actions.types'
+import {mapActions} from 'vuex'
+import {mapGetters} from 'vuex'
+
+export default {
+  computed:{
+    ...mapGetters([
+      'getName'
+    ]),
+  },
+  methods:{
+      ...mapActions([
+        'fetchPosts'
+      ])
+  },
+
+  created(){
+    this.$store.dispatch('fetchPosts')
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
