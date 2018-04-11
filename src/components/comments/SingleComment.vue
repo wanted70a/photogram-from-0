26988dc32b76927107c82d24689c02e05b1b9504
Stefan Milic <stefan.milic@diwanee.com>
2018-04-11 16:52:59 +0200

    update

--- src/components/comments/SingleComment.vue
+++ src/components/comments/SingleComment.vue
@@ -5,9 +5,21 @@
                 <img :src="IMG + comment.user_image.avatar" alt="">
             </router-link>
             <p class="mo-comment__text">{{comment.body}}</p>
-            <button class='mo-comment__edit' v-if='comment.user_id == getMyId' @click.stop='toggleEdit( )'><b>EDIT</b></button>
-            <p class='mo-comment__delete' v-if='comment.user_id == getMyId || comment.user_id == postUserId ' @click='deleteComment()'><b>X</b></p>
-            <input v-show='editMode' type="text" name="" v-model='input' @keyup.enter='submitEdit()'>
+            <div class='mo-comment__edit' v-if='comment.user_id == getMyId' @click.stop='toggleEdit( )'>
+                <div class="mo-comment__edit__icon">
+                    <svg class="mo-comment__edit__icon--pen"xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
+                        <path d="M402.6 83.2l90.2 90.2c3.8 3.8 3.8 10 0 13.8L274.4 405.6l-92.8 10.3c-12.4 1.4-22.9-9.1-21.5-21.5l10.3-92.8L388.8 83.2c3.8-3.8 10-3.8 13.8 0zm162-22.9l-48.8-48.8c-15.2-15.2-39.9-15.2-55.2 0l-35.4 35.4c-3.8 3.8-3.8 10 0 13.8l90.2 90.2c3.8 3.8 10 3.8 13.8 0l35.4-35.4c15.2-15.3 15.2-40 0-55.2zM384 346.2V448H64V128h229.8c3.2 0 6.2-1.3 8.5-3.5l40-40c7.6-7.6 2.2-20.5-8.5-20.5H48C21.5 64 0 85.5 0 112v352c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V306.2c0-10.7-12.9-16-20.5-8.5l-40 40c-2.2 2.3-3.5 5.3-3.5 8.5z"/>
+                    </svg>
+                </div>
+            </div>
+            <div class='mo-comment__delete' v-if='comment.user_id == getMyId || comment.user_id == postUserId ' @click='deleteComment()'>
+                <div class="mo-comment__delete__icon">
+                    <svg class="mo-comment__delete__icon--trash"xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                        <path d="M0 84V56c0-13.3 10.7-24 24-24h112l9.4-18.7c4-8.2 12.3-13.3 21.4-13.3h114.3c9.1 0 17.4 5.1 21.5 13.3L312 32h112c13.3 0 24 10.7 24 24v28c0 6.6-5.4 12-12 12H12C5.4 96 0 90.6 0 84zm415.2 56.7L394.8 467c-1.6 25.3-22.6 45-47.9 45H101.1c-25.3 0-46.3-19.7-47.9-45L32.8 140.7c-.4-6.9 5.1-12.7 12-12.7h358.5c6.8 0 12.3 5.8 11.9 12.7z"/>
+                    </svg>
+                </div>
+            </div>
+            <input v-show='editMode' ref="editComment" type="text" name="" v-model='input' @keyup.enter='submitEdit()'>
         </div>
     </transition>
 </template>
@@ -31,11 +43,18 @@ export default {
     methods:{
         toggleEdit( ){
             this.editMode = !this.editMode;
+            this.$nextTick(() => this.$refs.editComment.focus())
+            console.log(this.$refs.editComment.focus);
         },
 
         submitEdit( initialVal ){
             if( this.input === this.$props.comment.body ){
                 //if data did not change do not fetch
+                this.editMode = false;
+                return;
+            }
+            else if( this.input === ''){
+                this.input = this.$props.comment.body
                 this.editMode = false;
                 return;
             }
--- src/components/posts/SinglePost.vue
+++ src/components/posts/SinglePost.vue
@@ -11,11 +11,18 @@
 
             <div class="mo-post__media" @click='showPostDetails( index )'>
                 <div class="mo-post__delete">
-                    <p v-if='post.user_id == getMyId && !deleteMode' @click.stop='toggleDelete'><b>X</b></p>
-                    <div v-if='deleteMode'  class="mo-post__delete__dialog">
-                        <p>Sure To Delete</p>
-                        <button type="button" class='c-btn c-btn--delete-post'  @click.stop='deletePost( post.id )' >YES</button>
-                        <button type="button" class='c-btn c-btn--delete-post' @click.stop='toggleDelete'>NO</button>
+                    <div class="mo-post__delete__icon" v-if='post.user_id == getMyId && !deleteMode' @click.stop='toggleDelete'>
+                        <svg class="mo-post__delete__icon--trash"xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                            <path d="M0 84V56c0-13.3 10.7-24 24-24h112l9.4-18.7c4-8.2 12.3-13.3 21.4-13.3h114.3c9.1 0 17.4 5.1 21.5 13.3L312 32h112c13.3 0 24 10.7 24 24v28c0 6.6-5.4 12-12 12H12C5.4 96 0 90.6 0 84zm415.2 56.7L394.8 467c-1.6 25.3-22.6 45-47.9 45H101.1c-25.3 0-46.3-19.7-47.9-45L32.8 140.7c-.4-6.9 5.1-12.7 12-12.7h358.5c6.8 0 12.3 5.8 11.9 12.7z"/>
+                        </svg>
+                    </div>
+                    <div v-if='deleteMode'  class="mo-post__delete__dialog" @click.stop=''>
+                        <div class='mo-post__delete__dialog__inner'>
+                            <p>Sure To Delete ?</p>
+                            <button type="button" class='c-btn c-btn--delete-post'  @click.stop='deletePost( post.id )' >YES</button>
+                            <button type="button" class='c-btn c-btn--delete-post' @click.stop='toggleDelete'>NO</button>
+                        </div>
+
                     </div>
                 </div>
 
@@ -28,43 +35,19 @@
 
             <div class="mo-post__stats">
                 <div class='mo-post__stats__icon' @click='showPostComments( index )' >
-                    <svg class='mo-post__stats__comment' version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-                    	 width="511.626px" height="511.627px" viewBox="0 0 511.626 511.627" style="enable-background:new 0 0 511.626 511.627;"
-                    	 xml:space="preserve">
-                    	<path d="M477.364,127.481c-22.839-28.072-53.864-50.248-93.072-66.522c-39.208-16.274-82.036-24.41-128.479-24.41
-                    		c-46.442,0-89.269,8.136-128.478,24.41c-39.209,16.274-70.233,38.446-93.074,66.522C11.419,155.555,0,186.15,0,219.269
-                    		c0,28.549,8.61,55.299,25.837,80.232c17.227,24.934,40.778,45.874,70.664,62.813c-2.096,7.611-4.57,14.842-7.426,21.7
-                    		c-2.855,6.851-5.424,12.467-7.708,16.847c-2.286,4.374-5.376,9.23-9.281,14.555c-3.899,5.332-6.849,9.093-8.848,11.283
-                    		c-1.997,2.19-5.28,5.801-9.851,10.848c-4.565,5.041-7.517,8.33-8.848,9.853c-0.193,0.097-0.953,0.948-2.285,2.574
-                    		c-1.331,1.615-1.999,2.419-1.999,2.419l-1.713,2.57c-0.953,1.42-1.381,2.327-1.287,2.703c0.096,0.384-0.094,1.335-0.57,2.854
-                    		c-0.477,1.526-0.428,2.669,0.142,3.429v0.287c0.762,3.234,2.283,5.853,4.567,7.851c2.284,1.992,4.858,2.991,7.71,2.991h1.429
-                    		c12.375-1.526,23.223-3.613,32.548-6.279c49.87-12.751,93.649-35.782,131.334-69.094c14.274,1.523,28.074,2.283,41.396,2.283
-                    		c46.442,0,89.271-8.135,128.479-24.414c39.208-16.276,70.233-38.444,93.072-66.517c22.843-28.072,34.263-58.67,34.263-91.789
-                    		C511.626,186.154,500.207,155.555,477.364,127.481z M445.244,292.075c-19.896,22.456-46.733,40.303-80.517,53.529
-                    		c-33.784,13.223-70.093,19.842-108.921,19.842c-11.609,0-23.98-0.76-37.113-2.286l-16.274-1.708l-12.277,10.852
-                    		c-23.408,20.558-49.582,36.829-78.513,48.821c8.754-15.414,15.416-31.785,19.986-49.102l7.708-27.412l-24.838-14.27
-                    		c-24.744-14.093-43.918-30.793-57.53-50.114c-13.61-19.315-20.412-39.638-20.412-60.954c0-26.077,9.945-50.343,29.834-72.803
-                    		c19.895-22.458,46.729-40.303,80.515-53.531c33.786-13.229,70.089-19.849,108.92-19.849c38.828,0,75.13,6.617,108.914,19.845
-                    		c33.783,13.229,60.62,31.073,80.517,53.531c19.89,22.46,29.834,46.727,29.834,72.802S465.133,269.615,445.244,292.075z"/>
+                    <svg class='mo-post__stats__comment' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                        <path d="M256 32C114.6 32 0 125.1 0 240c0 49.6 21.4 95 57 130.7C44.5 421.1 2.7 466 2.2 466.5c-2.2 2.3-2.8 5.7-1.5 8.7S4.8 480 8 480c66.3 0 116-31.8 140.6-51.4 32.7 12.3 69 19.4 107.4 19.4 141.4 0 256-93.1 256-208S397.4 32 256 32z"/>
                     </svg>
                     {{post.comments_count}}
                 </div>
                 <div class='mo-post__stats__icon'>
-                    <svg class=' mo-post__stats__like' version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-                    	 viewBox="0 0 51.997 51.997" style="enable-background:new 0 0 51.997 51.997;" xml:space="preserve" >
-                    <path d="M51.911,16.242C51.152,7.888,45.239,1.827,37.839,1.827c-4.93,0-9.444,2.653-11.984,6.905
-                    	c-2.517-4.307-6.846-6.906-11.697-6.906c-7.399,0-13.313,6.061-14.071,14.415c-0.06,0.369-0.306,2.311,0.442,5.478
-                    	c1.078,4.568,3.568,8.723,7.199,12.013l18.115,16.439l18.426-16.438c3.631-3.291,6.121-7.445,7.199-12.014
-                    	C52.216,18.553,51.97,16.611,51.911,16.242z"/>
+                    <svg class=' mo-post__stats__like' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                        <path d="M462.3 62.6C407.5 15.9 326 24.3 275.7 76.2L256 96.5l-19.7-20.3C186.1 24.3 104.5 15.9 49.7 62.6c-62.8 53.6-66.1 149.8-9.9 207.9l193.5 199.8c12.5 12.9 32.8 12.9 45.3 0l193.5-199.8c56.3-58.1 53-154.3-9.8-207.9z"/>
                     </svg>
                 </div>
                 <div class='mo-post__stats__icon mo-post__stats__icon--last'>
-                    <svg class=' mo-post__stats__liked' version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-                    	 viewBox="0 0 51.997 51.997" style="enable-background:new 0 0 51.997 51.997;" xml:space="preserve" >
-                    <path d="M51.911,16.242C51.152,7.888,45.239,1.827,37.839,1.827c-4.93,0-9.444,2.653-11.984,6.905
-                    	c-2.517-4.307-6.846-6.906-11.697-6.906c-7.399,0-13.313,6.061-14.071,14.415c-0.06,0.369-0.306,2.311,0.442,5.478
-                    	c1.078,4.568,3.568,8.723,7.199,12.013l18.115,16.439l18.426-16.438c3.631-3.291,6.121-7.445,7.199-12.014
-                    	C52.216,18.553,51.97,16.611,51.911,16.242z"/>
+                    <svg class=' mo-post__stats__liked' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                        <path d="M462.3 62.6C407.5 15.9 326 24.3 275.7 76.2L256 96.5l-19.7-20.3C186.1 24.3 104.5 15.9 49.7 62.6c-62.8 53.6-66.1 149.8-9.9 207.9l193.5 199.8c12.5 12.9 32.8 12.9 45.3 0l193.5-199.8c56.3-58.1 53-154.3-9.8-207.9z"/>
                     </svg>
                     <span> {{post.likes_count}}</span>
                 </div>
@@ -83,10 +66,14 @@
             </div>
             <div class="mo-post__slider-controls">
                 <div class="mo-post__next" @click.prevent='nextPost( index )'>
-                    <button type="button" class='btn btn--next-post'>NEXT</button>
+                    <button type="button" class='c-btn--next-post'>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M34.52 239.03L228.87 44.69c9.37-9.37 24.57-9.37 33.94 0l22.67 22.67c9.36 9.36 9.37 24.52.04 33.9L131.49 256l154.02 154.75c9.34 9.38 9.32 24.54-.04 33.9l-22.67 22.67c-9.37 9.37-24.57 9.37-33.94 0L34.52 272.97c-9.37-9.37-9.37-24.57 0-33.94z"/></svg>
+                    </button>
                 </div>
                 <div class="mo-post__prev" @click.prevent='prevPost( index )'>
-                    <button type="button" class='btn btn--prev-post'>PREV</button>
+                    <button type="button" class='c-btn--prev-post'>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M34.52 239.03L228.87 44.69c9.37-9.37 24.57-9.37 33.94 0l22.67 22.67c9.36 9.36 9.37 24.52.04 33.9L131.49 256l154.02 154.75c9.34 9.38 9.32 24.54-.04 33.9l-22.67 22.67c-9.37 9.37-24.57 9.37-33.94 0L34.52 272.97c-9.37-9.37-9.37-24.57 0-33.94z"/></svg>
+                    </button>
                 </div>
             </div>
         </div>
@@ -100,6 +87,10 @@ import { mapGetters } from 'vuex'
 import {  UPDATE_CURRENT_POST_INDEX, UPDATE_POSTS, FETCH_POSTS, FETCH_POSTS_BY_USER_ID,POP_POST_BY_INDEX, UPDATE_COMMENTS, UPDATE_POST_DETAILS_STATE, UPDATE_COMMENTS_DETAILS_STATE, UPDATE_COMMENTS_RQST_PAGE, FETCH_COMMENTS, DELETE_POST_BY_ID, UPDATE_POSTS_RQST_PAGE } from '../../store/modules/actions.types'
 import SingleComment from '../comments/SingleComment.vue'
 import AddComment from '../comments/AddComment.vue'
+
+var w = window.innerWidth;
+console.log(w);
+
 export default {
     data(){
         return {
@@ -110,10 +101,11 @@ export default {
     },
     methods:{
         showPostDetails( index ){
-            this.$store.dispatch( UPDATE_CURRENT_POST_INDEX, index  );
-            this.$store.dispatch( UPDATE_COMMENTS_DETAILS_STATE, false  );
-            this.$store.dispatch( UPDATE_POST_DETAILS_STATE, true  );
-
+            if(w > 1024){
+                this.$store.dispatch( UPDATE_CURRENT_POST_INDEX, index  );
+                this.$store.dispatch( UPDATE_COMMENTS_DETAILS_STATE, false  );
+                this.$store.dispatch( UPDATE_POST_DETAILS_STATE, true  );
+            }
         },
         showPostComments( index ){
             this.$store.dispatch( UPDATE_CURRENT_POST_INDEX, index  );
@@ -177,8 +169,5 @@ export default {
 }
 </script>
 <style>
- .mo-post__delete p{
-     box-shadow: 2px 2px 2px rgba(0,0,0,0.5);
-     display: inline-block;
- }
+
 </style>
